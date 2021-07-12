1eecd04fff715ae38588c71bc42c875c6866ec1b
Anton Novosad <noobito.2011@gmail.com>
2021-07-12 20:58:36 +0300

    Initial commit

---
+++ src/main/java/by/teachmeskills/diplom/DiplomApplication.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.diplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/teachmeskills/diplom/DiplomApplicationTests.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.diplom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
